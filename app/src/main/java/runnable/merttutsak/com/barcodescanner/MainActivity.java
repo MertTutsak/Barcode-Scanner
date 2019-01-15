2a0d1fb9d4a93410d071625b68cff5e7915efd51
mert_tutsak <merttutsak@gmail.com>
2019-01-16 00:25:46 +0300

    first commit

---
+++ app/src/main/java/runnable/merttutsak/com/barcodescanner/MainActivity.java
@@ -0,0 +1,58 @@
+package runnable.merttutsak.com.barcodescanner;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.txtContent);
+        ImageView imageView = (ImageView) findViewById(R.id.imgview);
+        Button button = (Button) findViewById(R.id.button);
+
+        //Image
+        final Bitmap bitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.puppy);
+        imageView.setImageBitmap(bitmap);
+
+        //Button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //Barcode Detector
+                BarcodeDetector detector = new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+
+                if (!detector.isOperational()) {
+                    textView.setText("Could not set up the detector!");
+                    return;
+                }
+
+                Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                SparseArray<Barcode> barcodes = detector.detect(frame);
+
+                Barcode thisCode = barcodes.valueAt(0);
+                textView.setText(thisCode.rawValue);
+            }
+        });
+    }
+}
